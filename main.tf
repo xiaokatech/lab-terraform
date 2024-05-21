7726525641438a21a590bc2dd16d5d11406d6f5e
Jiean Yang <jiean.yang.freelancer@gmail.com>
2024-05-21 12:28:25 +0200

    Configure main.ts for resource

--- main.tf
+++ main.tf
@@ -10,6 +10,16 @@ terraform {
 # Configure the AWS Provider
 provider "aws" {
   region     = "us-east-1"
-  access_key = "my-access-key"
-  secret_key = "my-secret-key"
+  access_key = "access_key"
+  secret_key = "secret_key"
+}
+
+
+resource "aws_instance" "my-first-server" {
+  ami           = "ami-04b70fa74e45c3917"
+  instance_type = "t2.nano"
+
+  tags = {
+    Name = "to-delete"
+  }
 }
