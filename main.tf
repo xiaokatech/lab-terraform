8817cdb1e78e13126e5644da508d020e76a7d6f4
Jiean Yang <jiean.yang.freelancer@gmail.com>
2024-05-21 12:52:33 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,11 +7,24 @@ terraform {
   }
 }
 
+variable "AWS_REGION" {
+  type        = string
+  description = "AWS access key"
+}
+variable "AWS_ACCESS_KEY" {
+  type        = string
+  description = "AWS access key"
+}
+variable "AWS_SECRET_KEY" {
+  type        = string
+  description = "AWS secret key"
+}
+
 # Configure the AWS Provider
 provider "aws" {
-  region     = "us-east-1"
-  access_key = "access_key"
-  secret_key = "secret_key"
+  region     = var.AWS_REGION
+  access_key = var.AWS_ACCESS_KEY
+  secret_key = var.AWS_SECRET_KEY
 }
 
 
