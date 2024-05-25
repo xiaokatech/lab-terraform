319b04a2ea47869fafb457301d29dc76d44305a8
Jiean Yang <jiean.yang.freelancer@gmail.com>
2024-05-25 16:52:07 +0200

    Create subnet and associate route-table and subnet

--- main.tf
+++ main.tf
@@ -74,17 +74,25 @@ resource "aws_route_table" "prod-route-table" {
 }
 
 # 4. Create a Subnet
-# 5. Associate subnet with Route Table
-# 6. Create Security Group to allow port 20, 80 and 443
-# 7. Create a network interface with an ip in the subnet that was created in step 4
-# 8. Assign an elastic IP to the network interface created in step 7
-# 9. Create a ubuntu server and install/enable apache2
-
 resource "aws_subnet" "subnet-1" {
-  vpc_id     = aws_vpc.first-vpc.id
-  cidr_block = "10.0.1.0/24"
+  vpc_id            = aws_vpc.first-vpc.id
+  cidr_block        = "10.0.1.0/24"
+  availability_zone = "us-east-1a"
 
   tags = {
     Name = "lab-terraform"
   }
 }
+
+
+# 5. Associate subnet with Route Table
+resource "aws_route_table_association" "a" {
+  subnet_id      = aws_subnet.subnet-1.id
+  route_table_id = aws_route_table.prod-route-table.id
+}
+
+# 6. Create Security Group to allow port 20, 80 and 443
+# 7. Create a network interface with an ip in the subnet that was created in step 4
+# 8. Assign an elastic IP to the network interface created in step 7
+# 9. Create a ubuntu server and install/enable apache2
+
