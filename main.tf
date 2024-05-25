71b17a5e757844eea33d6d3af95eb8bf6d343843
Jiean Yang <jiean.yang.freelancer@gmail.com>
2024-05-25 16:44:49 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -36,13 +36,49 @@ provider "aws" {
 #   }
 # }
 
-resource "aws_vpc" "first-vpc" {
+# 1. Create a VPC
+resource "aws_vpc" "prod-vpc" {
   cidr_block = "10.0.0.0/16"
 
   tags = {
     Name = "lab-terraform"
   }
 }
+
+# 2. Create an Internet Gateway
+resource "aws_internet_gateway" "gw" {
+  vpc_id = aws_vpc.prod-vpc.id
+
+  tags = {
+    Name = "lab-terraform"
+  }
+}
+
+# 3. Create a Route Table
+resource "aws_route_table" "prod-route-table" {
+  vpc_id = aws_vpc.prod-vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.gw.id
+  }
+
+  route {
+    ipv6_cidr_block        = "::/0"
+    egress_only_gateway_id = aws_internet_gateway.gw.id
+  }
+
+  tags = {
+    Name = "lab-terraform"
+  }
+}
+
+# 4. Create a Subnet
+# 5. Associate subnet with Route Table
+# 6. Create Security Group to allow port 20, 80 and 443
+# 7. Create a network interface with an ip in the subnet that was created in step 4
+# 8. Assign an elastic IP to the network interface created in step 7
+# 9. Create a ubuntu server and install/enable apache2
 
 resource "aws_subnet" "subnet-1" {
   vpc_id     = aws_vpc.first-vpc.id
