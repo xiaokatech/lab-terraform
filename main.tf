464768e87fb574a433d9b7ff8295fdd368c29047
Jiean Yang <jiean.yang.freelancer@gmail.com>
2024-05-25 17:02:41 +0200

    Create AWS Security group

--- main.tf
+++ main.tf
@@ -75,7 +75,7 @@ resource "aws_route_table" "prod-route-table" {
 
 # 4. Create a Subnet
 resource "aws_subnet" "subnet-1" {
-  vpc_id            = aws_vpc.first-vpc.id
+  vpc_id            = aws_vpc.prod-vpc.id
   cidr_block        = "10.0.1.0/24"
   availability_zone = "us-east-1a"
 
@@ -92,6 +92,47 @@ resource "aws_route_table_association" "a" {
 }
 
 # 6. Create Security Group to allow port 20, 80 and 443
+resource "aws_security_group" "allow_web" {
+  name        = "allow_web"
+  description = "Allow web inbound traffic"
+  vpc_id      = aws_vpc.prod-vpc.id
+
+  ingress {
+    description = "HTTPS traffic"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "HTTP traffic"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "lab-terraform"
+  }
+}
+
 # 7. Create a network interface with an ip in the subnet that was created in step 4
 # 8. Assign an elastic IP to the network interface created in step 7
 # 9. Create a ubuntu server and install/enable apache2
