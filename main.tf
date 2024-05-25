db4f4ce36de9f866106f17283b97e999c03aebfb
Jiean Yang <jiean.yang.freelancer@gmail.com>
2024-05-25 17:47:08 +0200

    Try to debug main.tf

--- main.tf
+++ main.tf
@@ -147,7 +147,7 @@ resource "aws_eip" "one" {
   domain                    = "vpc"
   network_interface         = aws_network_interface.web-server-nic.id
   associate_with_private_ip = "10.0.1.50"
-  depends_on                = [aws_internet_gateway.gw]
+  depends_on                = [aws_internet_gateway.gw, aws_network_interface.web-server-nic]
 
   tags = {
     Name = "lab-terraform"
